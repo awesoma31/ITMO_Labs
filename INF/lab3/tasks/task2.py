ccc5e403e6c8257db0066544344744e27bbdcefe
awesoma31 <gwerty31102005@gmail.com>
2023-10-09 10:42:16 +0300

    -

--- INF/lab3/tasks/task2.py
+++ INF/lab3/tasks/task2.py
@@ -7,10 +7,18 @@ def solve(string):
     затем не более 4 слов, и после этого идёт слово «ИТМО».
     Для простоты будем считать словом любую последовательность букв, цифр и знаков «_» (то есть символов \w).
 
+    г. Москва, ул. Залупина, д. 100
+
     :param string:
     :return: res: list of fragments found in the text
     """
-    pattern = re.compile(r"ВТ(?:\W*[\s-]+\w+){0,4}[\s-]+ИТМО")
+    pattern = re.compile(r"ВТ\W*(?:[\s-]+\w+){0,4}[\s-]+ИТМО")
+
     res = pattern.findall(string)
+    for i in range(len(res)):
+        res[i] = re.sub(r'[-\$\?!]\s?', '', res[i])
 
     return res
+
+data = 'а вы значли, что ВТ - лучшая кафедра в ИТМО'
+print(solve(data))
