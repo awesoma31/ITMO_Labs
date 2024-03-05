74b3de1fee757e423abfa21e71f09e0029ae02d8
Alexander Churakov <gwerty31102005@gmail.com>
2024-03-05 15:03:27 +0300

    -

---
+++ PROG/lab5/docs/javadoc/member-search-index.js
@@ -0,0 +1,562 @@
+memberSearchIndex = [{
+    "p": "awesoma.common.commands",
+    "c": "Add",
+    "l": "Add(BufferedReader, UniqueIdGenerator, Vector<Movie>)",
+    "url": "%3Cinit%3E(java.io.BufferedReader,awesoma.common.util.UniqueIdGenerator,java.util.Vector)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "AddIfMax",
+    "l": "AddIfMax(BufferedReader, UniqueIdGenerator, Vector<Movie>)",
+    "url": "%3Cinit%3E(java.io.BufferedReader,awesoma.common.util.UniqueIdGenerator,java.util.Vector)"
+}, {"p": "awesoma.common.commands", "c": "Command", "l": "argAmount"}, {
+    "p": "awesoma.common.commands",
+    "c": "ExecuteScript",
+    "l": "argAmount"
+}, {"p": "awesoma.common.commands", "c": "Exit", "l": "argAmount"}, {
+    "p": "awesoma.common.commands",
+    "c": "Help",
+    "l": "argAmount"
+}, {"p": "awesoma.common.commands", "c": "Info", "l": "argAmount"}, {
+    "p": "awesoma.common.commands",
+    "c": "RemoveAt",
+    "l": "argAmount"
+}, {"p": "awesoma.common.commands", "c": "RemoveById", "l": "argAmount"}, {
+    "p": "awesoma.common.commands",
+    "c": "UpdateId",
+    "l": "argAmount"
+}, {
+    "p": "awesoma.common.exceptions",
+    "c": "ArgParsingException",
+    "l": "ArgParsingException()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.exceptions",
+    "c": "ArgParsingException",
+    "l": "ArgParsingException(String)",
+    "url": "%3Cinit%3E(java.lang.String)"
+}, {"p": "awesoma.common.models", "c": "Color", "l": "BLACK"}, {
+    "p": "awesoma.common.commands",
+    "c": "Clear",
+    "l": "Clear(Vector<Movie>)",
+    "url": "%3Cinit%3E(java.util.Vector)"
+}, {"p": "awesoma.common.models", "c": "MovieGenre", "l": "COMEDY"}, {
+    "p": "awesoma.common.commands",
+    "c": "Command",
+    "l": "Command(String, String)",
+    "url": "%3Cinit%3E(java.lang.String,java.lang.String)"
+}, {
+    "p": "awesoma.common.exceptions",
+    "c": "CommandExecutingException",
+    "l": "CommandExecutingException()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.exceptions",
+    "c": "CommandExecutingException",
+    "l": "CommandExecutingException(String)",
+    "url": "%3Cinit%3E(java.lang.String)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "compareTo(Movie)",
+    "url": "compareTo(awesoma.common.models.Movie)"
+}, {
+    "p": "awesoma.managers",
+    "c": "Console",
+    "l": "Console(Command[], BufferedReader, Vector<Movie>)",
+    "url": "%3Cinit%3E(awesoma.common.commands.Command[],java.io.BufferedReader,java.util.Vector)"
+}, {
+    "p": "awesoma.managers",
+    "c": "Console",
+    "l": "Console(HashMap<String, Command>, BufferedReader, Vector<Movie>)",
+    "url": "%3Cinit%3E(java.util.HashMap,java.io.BufferedReader,java.util.Vector)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Coordinates",
+    "l": "Coordinates()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Coordinates",
+    "l": "Coordinates(double, long)",
+    "url": "%3Cinit%3E(double,long)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "DumpManager",
+    "l": "DumpManager(String, Validator)",
+    "url": "%3Cinit%3E(java.lang.String,awesoma.common.util.json.Validator)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Person",
+    "l": "equals(Object)",
+    "url": "equals(java.lang.Object)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Add",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "AddIfMax",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Clear",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "ExecutAble",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "ExecuteScript",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Exit",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Help",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Info",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "PrintFieldAscendingTotalBoxOffice",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "PrintFieldDescendingGenre",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "PrintFieldDescendingUsaBoxOffice",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "RemoveAt",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "RemoveById",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Save",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Show",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Sort",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "UpdateId",
+    "l": "execute(ArrayList<String>)",
+    "url": "execute(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "ExecuteScript",
+    "l": "ExecuteScript()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "ExecuteScript",
+    "l": "ExecuteScript(Vector<Movie>, HashMap<String, Command>)",
+    "url": "%3Cinit%3E(java.util.Vector,java.util.HashMap)"
+}, {"p": "awesoma.common.commands", "c": "Exit", "l": "Exit()", "url": "%3Cinit%3E()"}, {
+    "p": "awesoma.common.commands",
+    "c": "Exit",
+    "l": "Exit(String, String)",
+    "url": "%3Cinit%3E(java.lang.String,java.lang.String)"
+}, {"p": "awesoma.common.models", "c": "Country", "l": "FRANCE"}, {
+    "p": "awesoma.common.util",
+    "c": "UniqueIdGenerator",
+    "l": "generateUniqueId()"
+}, {"p": "awesoma.common.models", "c": "Country", "l": "GERMANY"}, {
+    "p": "awesoma.common.models",
+    "c": "Person",
+    "l": "getBirthday()"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "ExecuteScript",
+    "l": "getCommand(String)",
+    "url": "getCommand(java.lang.String)"
+}, {
+    "p": "awesoma.managers",
+    "c": "Console",
+    "l": "getCommand(String)",
+    "url": "getCommand(java.lang.String)"
+}, {"p": "awesoma.common.models", "c": "Movie", "l": "getCoordinates()"}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "getCreationDate()"
+}, {"p": "awesoma.common.commands", "c": "Command", "l": "getDescription()"}, {
+    "p": "awesoma.common.models",
+    "c": "Person",
+    "l": "getEyeColor()"
+}, {"p": "awesoma.common.models", "c": "Movie", "l": "getGenre()"}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "getId()"
+}, {"p": "awesoma.common.util", "c": "UniqueIdGenerator", "l": "getIdList()"}, {
+    "p": "awesoma.common.commands",
+    "c": "Command",
+    "l": "getInfo()"
+}, {"p": "awesoma.common.commands", "c": "Command", "l": "getName()"}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "getName()"
+}, {"p": "awesoma.common.models", "c": "Person", "l": "getName()"}, {
+    "p": "awesoma.common.models",
+    "c": "Person",
+    "l": "getNationality()"
+}, {"p": "awesoma.common.models", "c": "Movie", "l": "getOperator()"}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "getOscarsCount()"
+}, {"p": "awesoma.managers", "c": "Console", "l": "getRegisteredCommands()"}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "getTotalBoxOffice()"
+}, {"p": "awesoma.common.models", "c": "Movie", "l": "getUsaBoxOffice()"}, {
+    "p": "awesoma.common.models",
+    "c": "Color",
+    "l": "getVals()"
+}, {"p": "awesoma.common.models", "c": "Country", "l": "getVals()"}, {
+    "p": "awesoma.common.models",
+    "c": "MovieGenre",
+    "l": "getVals()"
+}, {"p": "awesoma.common.models", "c": "Person", "l": "getWeight()"}, {
+    "p": "awesoma.common.models",
+    "c": "Coordinates",
+    "l": "getX()"
+}, {"p": "awesoma.common.models", "c": "Coordinates", "l": "getY()"}, {
+    "p": "awesoma.common.models",
+    "c": "Person",
+    "l": "hashCode()"
+}, {"p": "awesoma.common.commands", "c": "Help", "l": "Help()", "url": "%3Cinit%3E()"}, {
+    "p": "awesoma.common.commands",
+    "c": "Help",
+    "l": "Help(HashMap<String, Command>)",
+    "url": "%3Cinit%3E(java.util.HashMap)"
+}, {"p": "awesoma.common.models", "c": "MovieGenre", "l": "HORROR"}, {
+    "p": "awesoma.common.util",
+    "c": "UniqueIdGenerator",
+    "l": "identifyIds(Vector<Movie>)",
+    "url": "identifyIds(java.util.Vector)"
+}, {"p": "awesoma.common.commands", "c": "Add", "l": "idList"}, {
+    "p": "awesoma.common.commands",
+    "c": "AddIfMax",
+    "l": "idList"
+}, {
+    "p": "awesoma.common.exceptions",
+    "c": "InfiniteScriptCallLoopException",
+    "l": "InfiniteScriptCallLoopException()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.exceptions",
+    "c": "InfiniteScriptCallLoopException",
+    "l": "InfiniteScriptCallLoopException(String)",
+    "url": "%3Cinit%3E(java.lang.String)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Info",
+    "l": "Info(Vector<Movie>, Date)",
+    "url": "%3Cinit%3E(java.util.Vector,java.util.Date)"
+}, {"p": "awesoma.client", "c": "Client", "l": "initDate"}, {
+    "p": "awesoma.managers",
+    "c": "Console",
+    "l": "interactiveMode()"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "LocalDateTimeJson",
+    "l": "LocalDateTimeJson()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.client",
+    "c": "Client",
+    "l": "main(String[])",
+    "url": "main(java.lang.String[])"
+}, {
+    "p": "awesoma.server",
+    "c": "Server",
+    "l": "main(String[])",
+    "url": "main(java.lang.String[])"
+}, {"p": "awesoma.common.models", "c": "Coordinates", "l": "maxY"}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "Movie()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "Movie(Integer, String, Integer, int, Long, Coordinates, LocalDateTime, MovieGenre, Person)",
+    "url": "%3Cinit%3E(java.lang.Integer,java.lang.String,java.lang.Integer,int,java.lang.Long,awesoma.common.models.Coordinates,java.time.LocalDateTime,awesoma.common.models.MovieGenre,awesoma.common.models.Person)"
+}, {"p": "awesoma.common.models", "c": "MovieGenre", "l": "MUSICAL"}, {
+    "p": "awesoma.common.models",
+    "c": "Color",
+    "l": "ORANGE"
+}, {"p": "awesoma.common.models", "c": "Person", "l": "Person()", "url": "%3Cinit%3E()"}, {
+    "p": "awesoma.common.models",
+    "c": "Person",
+    "l": "Person(String, Date, float, Color, Country)",
+    "url": "%3Cinit%3E(java.lang.String,java.util.Date,float,awesoma.common.models.Color,awesoma.common.models.Country)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "PrintFieldAscendingTotalBoxOffice",
+    "l": "PrintFieldAscendingTotalBoxOffice(Vector<Movie>)",
+    "url": "%3Cinit%3E(java.util.Vector)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "PrintFieldDescendingGenre",
+    "l": "PrintFieldDescendingGenre(Vector<Movie>)",
+    "url": "%3Cinit%3E(java.util.Vector)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "PrintFieldDescendingUsaBoxOffice",
+    "l": "PrintFieldDescendingUsaBoxOffice(Vector<Movie>)",
+    "url": "%3Cinit%3E(java.util.Vector)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Quit",
+    "l": "Quit()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "LocalDateTimeJson",
+    "l": "read(JsonReader)",
+    "url": "read(com.google.gson.stream.JsonReader)"
+}, {"p": "awesoma.common.util.json", "c": "DumpManager", "l": "readCollection()"}, {
+    "p": "awesoma.common.util.json",
+    "c": "DumpManager",
+    "l": "readCollection(String)",
+    "url": "readCollection(java.lang.String)"
+}, {"p": "awesoma.common.models", "c": "Color", "l": "RED"}, {
+    "p": "awesoma.managers",
+    "c": "Console",
+    "l": "registerCommands(ArrayList<Command>)",
+    "url": "registerCommands(java.util.ArrayList)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "RemoveAt",
+    "l": "RemoveAt(Vector<Movie>)",
+    "url": "%3Cinit%3E(java.util.Vector)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "RemoveById",
+    "l": "RemoveById(Vector<Movie>)",
+    "url": "%3Cinit%3E(java.util.Vector)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Save",
+    "l": "Save(Vector<Movie>, DumpManager)",
+    "url": "%3Cinit%3E(java.util.Vector,awesoma.common.util.json.DumpManager)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "setCoordinates(Coordinates)",
+    "url": "setCoordinates(awesoma.common.models.Coordinates)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "setCreationDate(LocalDateTime)",
+    "url": "setCreationDate(java.time.LocalDateTime)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "setGenre(MovieGenre)",
+    "url": "setGenre(awesoma.common.models.MovieGenre)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "setId(Integer)",
+    "url": "setId(java.lang.Integer)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Command",
+    "l": "setName(String)",
+    "url": "setName(java.lang.String)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "setName(String)",
+    "url": "setName(java.lang.String)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "setOperator(Person)",
+    "url": "setOperator(awesoma.common.models.Person)"
+}, {"p": "awesoma.common.models", "c": "Movie", "l": "setOscarsCount(int)"}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "setOscarsCount(Integer)",
+    "url": "setOscarsCount(java.lang.Integer)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "ExecuteScript",
+    "l": "setRegisteredCommands(HashMap<String, Command>)",
+    "url": "setRegisteredCommands(java.util.HashMap)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Help",
+    "l": "setRegisteredCommands(HashMap<String, Command>)",
+    "url": "setRegisteredCommands(java.util.HashMap)"
+}, {"p": "awesoma.common.models", "c": "Movie", "l": "setTotalBoxOffice(int)"}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "setUsaBoxOffice(Long)",
+    "url": "setUsaBoxOffice(java.lang.Long)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Show",
+    "l": "Show(Vector<Movie>)",
+    "url": "%3Cinit%3E(java.util.Vector)"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "Sort",
+    "l": "Sort(Vector<Movie>)",
+    "url": "%3Cinit%3E(java.util.Vector)"
+}, {"p": "awesoma.common.models", "c": "Coordinates", "l": "toString()"}, {
+    "p": "awesoma.common.models",
+    "c": "Movie",
+    "l": "toString()"
+}, {"p": "awesoma.common.models", "c": "Person", "l": "toString()"}, {
+    "p": "awesoma.common.util",
+    "c": "UniqueIdGenerator",
+    "l": "UniqueIdGenerator(HashSet<Integer>)",
+    "url": "%3Cinit%3E(java.util.HashSet)"
+}, {"p": "awesoma.common.models", "c": "Country", "l": "UNITED_KINGDOM"}, {
+    "p": "awesoma.common.exceptions",
+    "c": "UnrecognisedCommandException",
+    "l": "UnrecognisedCommandException()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.commands",
+    "c": "UpdateId",
+    "l": "UpdateId(Vector<Movie>, BufferedReader)",
+    "url": "%3Cinit%3E(java.util.Vector,java.io.BufferedReader)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateCollection(Vector<Movie>)",
+    "url": "validateCollection(java.util.Vector)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateCoordinates(Vector<Movie>)",
+    "url": "validateCoordinates(java.util.Vector)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateCreationDate(Vector<Movie>)",
+    "url": "validateCreationDate(java.util.Vector)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateGenre(Vector<Movie>)",
+    "url": "validateGenre(java.util.Vector)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateId(Vector<Movie>)",
+    "url": "validateId(java.util.Vector)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateName(Vector<Movie>)",
+    "url": "validateName(java.util.Vector)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateOperator(Vector<Movie>)",
+    "url": "validateOperator(java.util.Vector)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateOscarsCount(Vector<Movie>)",
+    "url": "validateOscarsCount(java.util.Vector)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateTotalBoxOffice(Vector<Movie>)",
+    "url": "validateTotalBoxOffice(java.util.Vector)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "validateUsaBoxOffice(Vector<Movie>)",
+    "url": "validateUsaBoxOffice(java.util.Vector)"
+}, {
+    "p": "awesoma.common.exceptions",
+    "c": "ValidationException",
+    "l": "ValidationException(String)",
+    "url": "%3Cinit%3E(java.lang.String)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "Validator",
+    "l": "Validator()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Color",
+    "l": "valueOf(String)",
+    "url": "valueOf(java.lang.String)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "Country",
+    "l": "valueOf(String)",
+    "url": "valueOf(java.lang.String)"
+}, {
+    "p": "awesoma.common.models",
+    "c": "MovieGenre",
+    "l": "valueOf(String)",
+    "url": "valueOf(java.lang.String)"
+}, {"p": "awesoma.common.models", "c": "Color", "l": "values()"}, {
+    "p": "awesoma.common.models",
+    "c": "Country",
+    "l": "values()"
+}, {"p": "awesoma.common.models", "c": "MovieGenre", "l": "values()"}, {
+    "p": "awesoma.common.models",
+    "c": "Color",
+    "l": "WHITE"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "LocalDateTimeJson",
+    "l": "write(JsonWriter, LocalDateTime)",
+    "url": "write(com.google.gson.stream.JsonWriter,java.time.LocalDateTime)"
+}, {
+    "p": "awesoma.common.util.json",
+    "c": "DumpManager",
+    "l": "writeCollection(Vector<Movie>)",
+    "url": "writeCollection(java.util.Vector)"
+}, {
+    "p": "awesoma.common.exceptions",
+    "c": "WrongAmountOfArgumentsException",
+    "l": "WrongAmountOfArgumentsException()",
+    "url": "%3Cinit%3E()"
+}, {
+    "p": "awesoma.common.exceptions",
+    "c": "WrongAmountOfArgumentsException",
+    "l": "WrongAmountOfArgumentsException(String)",
+    "url": "%3Cinit%3E(java.lang.String)"
+}, {"p": "awesoma.common.models", "c": "Color", "l": "YELLOW"}]
---
+++ PROG/lab5/docs/javadoc/package-search-index.js
@@ -0,0 +1,4 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "url": "allpackages-index.html"
+}, {"l": "awesoma.client"}, {"l": "awesoma.common.commands"}, {"l": "awesoma.common.exceptions"}, {"l": "awesoma.common.models"}, {"l": "awesoma.common.util"}, {"l": "awesoma.common.util.json"}, {"l": "awesoma.managers"}, {"l": "awesoma.server"}]
