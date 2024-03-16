f550fe5eed174153503f7941d3f9784f4f293852
awesoma <gwerty31102005@gmail.com>
2024-03-16 21:57:05 +0300

    sdcvcx

---
+++ OPD/lab4/main.asm
@@ -0,0 +1,53 @@
+org 0x453
+
+cla
+st R
+ld Y
+push 
+call $f
+pop
+sub R
+st R 
+ld Z 
+dec
+push 
+call &f
+pop
+inc 
+sub R 
+st R 
+ld X
+inc
+push
+call &f
+pop
+inc
+sub R
+st R 
+hlt
+
+Z : word 1
+Y: word 1
+X: word 1
+R: word 0
+
+org 0x732
+
+f:
+	ld &1
+	bmi dflt
+	bzs dflt
+	cmp A
+	bge dflt
+	asl
+	asl
+	sub &1
+	add B
+	jump fini
+dflt: ld A
+fini: st &1
+	ret
+	
+A: word 0x0D0D
+B: word 0x006D
+	
--- PROG/lab6/lab-common/src/main/java/awesoma/common/exceptions/ConversionException.java
+++
@@ -1,7 +0,0 @@
-package awesoma.common.exceptions;
-
-public class ConversionException extends RuntimeException {
-    public ConversionException(String msg) {
-        super(msg);
-    }
-}
