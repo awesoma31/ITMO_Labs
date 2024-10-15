357855239c95c200c97a005de5fd988283c3126d
awesoma31 <gwerty31102005@gmail.com>
2024-10-15 19:03:04 +0300

    -

--- ProgrammingJava, Программирование/lab6/server/src/main/java/org/awesoma/server/App.java
+++ ProgrammingJava, Программирование/lab6/server/src/main/java/org/awesoma/server/App.java
@@ -3,11 +3,6 @@ package org.awesoma.server;
 import org.awesoma.common.Environment;
 
 public class App {
-    //    public static final String DB_URL = "jdbc:postgresql://pg:5432/studs";
-//    public static final String DB_PASSWD = "yONE%4288";
-//    public static final String USER = "pg";
-
-
     public static void main(String[] args) {
         new TCPServer(Environment.HOST, Environment.PORT).run();
     }
--- ProgrammingJava, Программирование/lab7/common/src/main/java/org/awesoma/common/Environment.java
+++ ProgrammingJava, Программирование/lab7/common/src/main/java/org/awesoma/common/Environment.java
@@ -1,85 +1,85 @@
-package org.awesoma.common;
+    package org.awesoma.common;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvException;
-import org.awesoma.common.commands.*;
+    import io.github.cdimascio.dotenv.Dotenv;
+    import io.github.cdimascio.dotenv.DotenvException;
+    import org.awesoma.common.commands.*;
 
-import java.util.HashMap;
-
-/**
- * This class represents common fields required for both sides
- */
-public class Environment {
-    public static int PORT = 8000;
-    public static String HOST = "localhost";
-    private static String DB_CONFIG_FILE_PATH = "db.cfg";
-    private static final String DEFAULT_DB_CONFIG_FILE_PATH = "db.cfg";
-    private static String DB_URL;
-    private static final HashMap<String, Command> AVAILABLE_COMMANDS = new HashMap<>();
-
-    // GOVNOCODE
-    static {
-        registerCommands();
-        loadDBURL();
-    }
+    import java.util.HashMap;
 
     /**
-     * loads DB URL from .env file
+     * This class represents common fields required for both sides
      */
-    private static void loadDBURL() {
-        try {
-            Dotenv dotenv = Dotenv.load();
-            DB_URL = dotenv.get("DB_URL");
-        } catch (DotenvException e) {
-            System.err.println("<.env> file not found");
-            System.exit(1);
+    public class Environment {
+        public static int PORT = 8000;
+        public static String HOST = "localhost";
+        private static String DB_CONFIG_FILE_PATH = "db.cfg";
+        private static final String DEFAULT_DB_CONFIG_FILE_PATH = "db.cfg";
+        private static String DB_URL;
+        private static final HashMap<String, Command> AVAILABLE_COMMANDS = new HashMap<>();
+
+        // GOVNOCODE
+        static {
+            registerCommands();
+            loadDBURL();
+        }
+
+        /**
+         * loads DB URL from .env file
+         */
+        private static void loadDBURL() {
+            try {
+                Dotenv dotenv = Dotenv.load();
+                DB_URL = dotenv.get("DB_URL");
+            } catch (DotenvException e) {
+                System.err.println("<.env> file not found");
+                System.exit(1);
+            }
+        }
+
+        public static void setPORT(int PORT) {
+            Environment.PORT = PORT;
+        }
+
+        public static void setHOST(String HOST) {
+            Environment.HOST = HOST;
+        }
+
+        public static HashMap<String, Command> getAvailableCommands() {
+            return AVAILABLE_COMMANDS;
+        }
+
+        public static void register(Command command) {
+            AVAILABLE_COMMANDS.put(command.getName(), command);
+        }
+
+        public static void registerCommands() {
+            register(new HelpCommand());
+            register(new ShowCommand());
+            register(new ExitCommand());
+            register(new AddCommand());
+            register(new InfoCommand());
+            register(new ClearCommand());
+            register(new SortCommand());
+            register(new PrintFieldAscendingTBOCommand());
+            register(new UpdateIdCommand());
+            register(new RemoveAtCommand());
+            register(new RemoveByIdCommand());
+            register(new AddIfMaxCommand());
+        }
+
+        public static String getDbConfigFilePath() {
+            return DB_CONFIG_FILE_PATH;
+        }
+
+        public static void setDbConfigFilePath(String dbConfigFilePath) {
+            DB_CONFIG_FILE_PATH = dbConfigFilePath;
+        }
+
+        public static String getDefaultDbConfigFilePath() {
+            return DEFAULT_DB_CONFIG_FILE_PATH;
+        }
+
+        public static String getDbUrl() {
+            return DB_URL;
         }
     }
-
-    public static void setPORT(int PORT) {
-        Environment.PORT = PORT;
-    }
-
-    public static void setHOST(String HOST) {
-        Environment.HOST = HOST;
-    }
-
-    public static HashMap<String, Command> getAvailableCommands() {
-        return AVAILABLE_COMMANDS;
-    }
-
-    public static void register(Command command) {
-        AVAILABLE_COMMANDS.put(command.getName(), command);
-    }
-
-    public static void registerCommands() {
-        register(new HelpCommand());
-        register(new ShowCommand());
-        register(new ExitCommand());
-        register(new AddCommand());
-        register(new InfoCommand());
-        register(new ClearCommand());
-        register(new SortCommand());
-        register(new PrintFieldAscendingTBOCommand());
-        register(new UpdateIdCommand());
-        register(new RemoveAtCommand());
-        register(new RemoveByIdCommand());
-        register(new AddIfMaxCommand());
-    }
-
-    public static String getDbConfigFilePath() {
-        return DB_CONFIG_FILE_PATH;
-    }
-
-    public static void setDbConfigFilePath(String dbConfigFilePath) {
-        DB_CONFIG_FILE_PATH = dbConfigFilePath;
-    }
-
-    public static String getDefaultDbConfigFilePath() {
-        return DEFAULT_DB_CONFIG_FILE_PATH;
-    }
-
-    public static String getDbUrl() {
-        return DB_URL;
-    }
-}
