4de66a43a8db76cadd076a6323f2f7aba97eff02
awesoma31 <gwerty31102005@gmail.com>
2024-12-06 16:26:46 +0300

    sem7

---
+++ PL, ЯП/sem7/task2-3/stack-smash-fixed.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct user {
+  const char *name, *password;
+} const users[] = {{"Cat", "Meowmeow"}, {"Skeletor", "Nyarr"}};
+
+void print_users() {
+  printf("Users:\n");
+  for (size_t i = 0; i < sizeof(users) / sizeof(struct user); i++) {
+    printf("%s: %s\n", users[i].name, users[i].password);
+  }
+}
+
+void fill(FILE *f, char *where, size_t max_size) {
+  size_t read_total = 0;
+  while (read_total < max_size - 1) {
+    size_t read = fread(where + read_total, 1, 1, f);
+    if (!read)
+      break;
+    read_total += read;
+  }
+  where[read_total] = '\0';
+}
+
+void vulnerable(FILE *f) {
+  char buffer[8];
+  fill(f, buffer, sizeof(buffer));
+}
+
+int main(int argc, char **argv) {
+  vulnerable(stdin);
+  puts("nothing happened");
+  return 0;
+}
+
---
+++ PL, ЯП/sem7/task4/fixed.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int check_password(FILE *f, const char *password) {
+  char buffer[11];
+  int okay = 0;
+
+ if (fgets(buffer, sizeof(buffer), f)) {
+   buffer[strcspn(buffer, "\n")] = '\0';
+
+    if (strcmp(buffer, password) == 0)
+      okay = 1;
+  }
+
+  return okay;
+}
+
+int main(int argc, char **argv) {
+  if (check_password(stdin, "password"))
+    puts("Access granted.");
+  else
+    puts("Wrong password.");
+}
+
