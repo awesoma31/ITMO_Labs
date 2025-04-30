7231a2d376e64bfff8ce00fbef55d098956c5bd5
awesoma31 <gwerty31102005@gmail.com>
2025-04-30 17:34:42 +0300

    (CSA) lab3 acc fixes

--- CSA/lab3/tasks/acc32/sum_word_cstream.s
+++ CSA/lab3/tasks/acc32/sum_word_cstream.s
@@ -1,49 +1,50 @@
-; sum+word_cstream
-; https://wrench.edu.swampbuds.me/result/c65357c8-535f-4ab1-b685-0a13c424dbb5
     .data
 SUM_LOW:         .word  0
 SUM_HIGH:        .word  0
 TEMP:            .word  0
+INP_ADDR:        .word  0x80
+OUT_ADDR:        .word  0x84
+INVALID:         .word  0xFFFFFFFF
+OF_OUT:          .word  0xCCCCCCCC
 ONE:             .word  1
 
     .text
-.org 0x90
 _start:
 LOOP:
-    load_addr    0x80                      
-    beqz         END                      
-    store_addr TEMP
-    add          SUM_LOW               
+    load_ind     INP_ADDR
+    beqz         END
+    store_addr   TEMP
+    add          SUM_LOW
     store_addr   SUM_LOW
 
-    load_addr TEMP
-    bgt POSITIVE
-    beqz POSITIVE
-    bvc LOOP
+    load_addr    TEMP
+    bgt          POSITIVE
+    beqz         POSITIVE
+    bvc          LOOP
 POSITIVE:
     bcc          LOOP
     load_addr    SUM_HIGH
     add          ONE
-    bvs          OVERFLOW           
+    bvs          OVERFLOW
     store_addr   SUM_HIGH
-    jmp LOOP
+    jmp          LOOP
 
 END:
     load_addr    SUM_HIGH
-    store_addr   0x84                      
+    store_ind    OUT_ADDR
     load_addr    SUM_LOW
-    store_addr   0x84                     
+    store_ind    OUT_ADDR
     halt
 
 INVALID_INPUT:
-    load_imm     0xFFFFFFFF              
-    store_addr   0x84                   
+    load_ind     INVALID
+    store_ind    OUT_ADDR
     halt
 
 OVERFLOW:
-    load_imm     0xCCCCCCCC            
-    store_addr   0x84                 
-    load_imm     0xCCCCCCCC
-    store_addr   0x84                
+    load_ind     OF_OUT
+    store_ind    OUT_ADDR
+    load_ind     OF_OUT
+    store_ind    OUT_ADDR
     halt
 
