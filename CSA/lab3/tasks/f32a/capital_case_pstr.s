8d79b92ed4a07cd69b6d96cff1c6f6b7439445c2
awesoma31 <gwerty31102005@gmail.com>
2025-04-30 17:57:52 +0300

    (CSA) -

--- CSA/lab3/tasks/f32a/capital_case_pstr.s
+++ CSA/lab3/tasks/f32a/capital_case_pstr.s
@@ -1,9 +1,8 @@
-\ https://wrench.edu.swampbuds.me/result/2ba1a637-7baa-4243-b613-7c0cc44126d0
     .data
 len:             .byte  0
 buf:             .byte  '_______________________________'
 padding:         .byte  '___'
-flag:            .word  1     
+flag:            .word  1
 input_addr:      .word  0x80
 output_addr:     .word  0x84
 mask:            .byte  0, '___'
@@ -19,13 +18,13 @@ _start:
 
     @p output_addr b!        \ b = output_addr
     lit len a!               \ a = len (длина полученной строки)
-    @+ lit 255 and   
+    @+ lit 255 and
 
     print
     halt
 
 capcase:
-    @b lit 255 and dup    
+    @b lit 255 and dup
     lit -10 + if end
     dup lit -97 + -if char_ab97
 
@@ -43,7 +42,7 @@ char_ab97:
     downcase_char ;
 
 downcase_char:
-    @p flag             
+    @p flag
     lit -1 + if do_upcase
 
     write_char
@@ -59,7 +58,7 @@ write_without_downcase:
     capcase ;
 
 do_upcase:
-    dup lit -32 +      
+    dup lit -32 +
     write_char
     lit 0 !p flag
     capcase ;
@@ -71,15 +70,15 @@ do_downcase:
     capcase ;
 
 write_char:
-    @p mask +                
-    !+                      
-    @p len                
-    lit 1 +              
-    dup                 
-    !p len             
-    lit 255 and       
-    lit -32 + if err 
-    ;               
+    @p mask +
+    !+
+    @p len
+    lit 1 +
+    dup
+    !p len
+    lit 255 and
+    lit -32 + if err
+    ;
 
 err:
     @p bullshit
