0cf0c14353272682b4292d63776e818894d1ecaf
awesoma31 <gwerty31102005@gmail.com>
2025-05-25 13:25:22 +0300

    (csa) lab3 - fix pascal style

--- CSA/lab3/tasks/f32a/capital_case_pstr.s
+++ CSA/lab3/tasks/f32a/capital_case_pstr.s
@@ -1,23 +1,22 @@
     .data
-len:             .byte  0
-buf:             .byte  '_______________________________'
+buf:             .byte  0, '_______________________________'
 padding:         .byte  '___'
 flag:            .word  1
 input_addr:      .word  0x80
 output_addr:     .word  0x84
 mask:            .byte  0, '___'
-bullshit:        .word  0xCCCCCCCC
+bad_output:      .word  0xCCCCCCCC
 
     .text
 
     .org 0x90
 _start:
     @p input_addr b!         \ b = input_addr
-    lit buf a!               \ a = buf
+    lit buf lit 1 + a!       \ a = buf
     capcase
 
     @p output_addr b!        \ b = output_addr
-    lit len a!               \ a = len (длина полученной строки)
+    lit buf a!               \ a = buf (длина полученной строки)
     @+ lit 255 and
 
     print
@@ -29,33 +28,31 @@ capcase:
     dup lit -97 + -if char_ab97
 
     dup lit -65 + -if char_ab65
-    shit_char ;
+    bad_char ;
+end:
+    drop
+    ;
+
+char_ab97:
+    dup lit -122 + -if bad_char
+    downcase_char ;
 char_ab65:
-    dup lit -90 + -if shit_char
+    dup lit -90 + -if bad_char
     upcase_char ;
-shit_char:
+bad_char:
     set_flag_1
     write_char
     capcase ;
-char_ab97:
-    dup lit -122 + -if shit_char
-    downcase_char ;
 
 downcase_char:
     @p flag
     lit -1 + if do_upcase
-
     write_char
     capcase ;
-
 upcase_char:
     @p flag
     lit -1 + if write_without_downcase
     do_downcase ;
-write_without_downcase:
-    write_char
-    lit 0 !p flag
-    capcase ;
 
 do_upcase:
     dup lit -32 +
@@ -69,26 +66,32 @@ do_downcase:
     lit 0 !p flag
     capcase ;
 
+    \ st: char to write
+    \ A: buf+1 - string in buf
 write_char:
-    @p mask +
-    !+
-    @p len
-    lit 1 +
-    dup
-    !p len
-    lit 255 and
+    @p mask +                \ mask char
+    !+                       \ mem[A] <- dataStack.pop(); A <- A + 1 \ char to ouput
+    @p buf lit 1 +           \ buf++
+    dup                      \ st: buf+1, buf+1
+    !p buf                   \ mem[<address>] <- dataStack.pop() \ store buf \ st: buf+1
+    lit 255 and              \ check if buf out range
     lit -32 + if err
     ;
+write_without_downcase:
+    write_char
+    lit 0 !p flag
+    capcase ;
 
 err:
-    @p bullshit
+    @p bad_output
     @p output_addr b!
     !b
     halt
-end:
-    drop
-    ;
 
+    \ b = output_addr
+    \ lit buf a!               \ a = buf (длина полученной строки)
+    \ @+ lit 255 and \ dataStack.push(mem[A]); A <- A + 1 \ a указ на нчало строки
+    \ st: number of chars to write
 print:
     dup if end_print
     lit -1 +
